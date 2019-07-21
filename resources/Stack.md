b3e28665cea23a27677f33c291f594b9abce933d
winnieaz <zandrea418@gmail.com>
2019-07-21 17:03:08 +0800

    feat: 1001 & 相关整理更新

---
+++ resources/Stack.md
@@ -0,0 +1,19 @@
+栈
+====
+*数据结构（C++语言版）清华*
+
++ 栈的基本操作
+    1. push
+    2. pop
+    3. top
+    
++ [java Stack类](https://www.runoob.com/java/java-stack-class.html) 
+(*也可以自行用linkedlist实现*)
+    1. vector类的所有方法
+    2. push() , pop() ,peek()
+
++ 栈擅长解决的典型问题：
+   1. 有明确的算法，解答以线性形式给出
+   2. 无论是递归还是迭代，逆序计算出
+   3. 输入输出规模不确定，难以事先确定容器大小
+
---
+++ src/StacksAndQueues/MyStack1.java
@@ -0,0 +1,56 @@
+package StacksAndQueues;
+
+import java.util.Stack;
+
+/**
+ * Created by Wayne.A.Z on 2019-07-21.
+ * 程序员代码面试指南1001
+ * 实现O(1)时间内push(),pop(),getMin()操作的栈
+ * https://github.com/LyricYang/Internet-Recruiting-Algorithm-Problems/blob/master/CodeInterviewGuide/Chapter01_Stack%26Queue/Q1001.md
+ */
+public class MyStack1 {
+
+    Stack<Integer> stackData;
+    Stack<Integer> stackMin;
+
+    public MyStack1() {
+        this.stackData = new Stack<>();
+        this.stackMin = new  Stack<>();
+    }
+
+    public void push(int value) {
+        this.stackData.push(value);
+        if(this.stackMin.isEmpty()) this.stackMin.push(new Integer(value));
+        else if (value < this.stackMin.peek()) {
+            this.stackMin.push(value);
+        }
+    }
+
+    public int pop() {
+        if (this.stackData.isEmpty()) {
+            throw new RuntimeException("The stack is empty.");
+        }
+        if (this.stackData.peek() == this.stackMin.peek()) {
+            this.stackMin.pop();
+        }
+        return this.stackData.pop();
+    }
+
+    public int getMin() {
+        if (this.stackMin.isEmpty()) throw new RuntimeException("The stack is empty.");
+
+        return this.stackMin.peek();
+    }
+
+    public static void main(String[] args) {
+        MyStack1 stack1 = new MyStack1();
+        stack1.push(3);
+        System.out.println(stack1.getMin());
+        stack1.push(4);
+        System.out.println(stack1.getMin());
+        stack1.push(1);
+        System.out.println(stack1.getMin());
+        System.out.println(stack1.pop());
+        System.out.println(stack1.getMin());
+    }
+}
